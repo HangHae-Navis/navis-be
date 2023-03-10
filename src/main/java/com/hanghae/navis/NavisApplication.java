96b654b1474e13d79b0ec309e08ae42afaf32c6d
dkssudtp1 <dkssudtp1@naver.com>
2023-03-10 12:43:20 +0900

    Initial commit

---
+++ src/main/java/com/hanghae/navis/NavisApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.navis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/navis/NavisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.navis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
