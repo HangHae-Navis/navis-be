cf7c13d7feaeec9018f25c112cc60677ddc664f4
Chaeyi <chaeyi0318@gmail.com>
2023-03-17 20:07:32 +0900

    fix : 어노테이션 변경

--- src/main/java/com/hanghae/navis/board/dto/BoardListResponseDto.java
+++ src/main/java/com/hanghae/navis/board/dto/BoardListResponseDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 public class BoardListResponseDto {
     private Long id;
 
