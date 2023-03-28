423ca7b24a3efee6d7e28ffccfa320d4807fc6bc
dkssudtp1 <dkssudtp1@naver.com>
2023-03-28 16:06:56 +0900

    fix : 어드민, 서포터 투표옵션에 리턴값 옵션id 추가

--- src/main/java/com/hanghae/navis/vote/dto/OptionAdminResponseDto.java
+++ src/main/java/com/hanghae/navis/vote/dto/OptionAdminResponseDto.java
@@ -13,12 +13,14 @@ import java.util.List;
 @AllArgsConstructor
 public class OptionAdminResponseDto {
 
+    private Long optionId;
     private String option;
     private Long count;
     private List<PickUserInfoDto> pickUserInfoDtoList;
 
     public static OptionAdminResponseDto of(VoteOption voteOption, List<PickUserInfoDto> pickUserInfoDtoList) {
         return OptionAdminResponseDto.builder()
+                .optionId(voteOption.getId())
                 .option(voteOption.getOption())
                 .count(voteOption.getVoteRecordList() == null ? 0L : (long) voteOption.getVoteRecordList().size())
                 .pickUserInfoDtoList(pickUserInfoDtoList)
