2e4b37b9c621d8477bdaedf6f78505da18c73de6
dkssudtp1 <dkssudtp1@naver.com>
2023-04-08 04:48:47 +0900

    test : ver2

--- src/main/java/com/hanghae/navis/common/controller/NginxController.java
+++ src/main/java/com/hanghae/navis/common/controller/NginxController.java
@@ -19,7 +19,7 @@ public class NginxController {
     //무중단배포 테스트를 위한 version 확인용. 새로고침하면 return 값이 자동으로 바뀌는지
     @GetMapping("/version")
     public String checkVersion() {
-        return "ver1";
+        return "ver2";
     }
 
 }
