2ad2ef139d81222d743a4ca8b7dbf12f79ca464e
dkssudtp1 <dkssudtp1@naver.com>
2023-04-11 22:39:49 +0900

    fix : 알림 수정

--- src/main/java/com/hanghae/navis/notification/entity/NotificationType.java
+++ src/main/java/com/hanghae/navis/notification/entity/NotificationType.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum NotificationType {
 
-    HOMEWORK_POST("homework", "과제가 새로 올라왔습니다."),
+    HOMEWORK_POST("homework", "새로운 과제가 올라왔습니다."),
+
+    NOTICE_POST("homework", "새로운 공지가 올라왔습니다."),
     CHAT_POST("chat", "새로운 채팅이 왔습니다.");
 
     private final String type;
