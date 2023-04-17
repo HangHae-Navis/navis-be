3b736b7f3d3cfcd9b73e2cc4168e49ccd71aa2db
dkssudtp1 <dkssudtp1@naver.com>
2023-04-17 16:43:57 +0900

    fix : 프로필 업데이트 수정

--- src/main/java/com/hanghae/navis/user/dto/ProfileUpdateRequestDto.java
+++ src/main/java/com/hanghae/navis/user/dto/ProfileUpdateRequestDto.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.Size;
 @Getter
 public class ProfileUpdateRequestDto {
     private MultipartFile profileImage;
-    @NotNull
+
     private String nickname;
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 15자의 비밀번호여야 합니다.")
     @Schema(example = "Dkssud1!")
