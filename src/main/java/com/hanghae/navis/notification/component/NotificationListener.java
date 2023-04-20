3ee187d9dfdfbd621734c46fe5eb44199989dc95
dkssudtp1 <dkssudtp1@naver.com>
2023-04-20 12:56:55 +0900

    fix : notification 스케쥴 수정

--- src/main/java/com/hanghae/navis/notification/component/NotificationListener.java
+++ src/main/java/com/hanghae/navis/notification/component/NotificationListener.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class NotificationListener {
     private final EmitterRepository emitterRepository;
 
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 50000)
     public void checkEmitters() {
         // reconnect logic
         emitterRepository.closeAllEmitters();
